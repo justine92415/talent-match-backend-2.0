dd936a12468c346664344d8394fd5d0bb86ef838
justine92415 <justine92415@gmail.com>
2025-08-07 19:27:24 +0800

    ♻️ refactor: 移除非生產環境下載入 .env 檔的邏輯

--- config/index.ts
+++ config/index.ts
@@ -4,11 +4,6 @@ import dbConfig from './db'
 import webConfig from './web'
 import secretConfig from './secret'
 
-// 只有在非 production 環境下才載入 .env 檔
-if (process.env.NODE_ENV !== 'production') {
-  const result = dotenv.config()
-}
-
 const config: Config = {
   db: dbConfig,
   web: webConfig,
